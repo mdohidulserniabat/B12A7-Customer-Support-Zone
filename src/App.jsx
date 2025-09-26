30a708186b75770a87c063e2f49bd3dea6a51d90
mdohidulserniabat <ohiduli090@gmail.com>
2025-09-26 16:29:12 +0600

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import './App.css';
+import Banner from './components/Banner/Banner';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
     </>
   );
 }
